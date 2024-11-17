8c9ec207b55d82a8b7cda75c8396efba6b8f51f2
RedCrafters <redcraft852@gmail.com>
2024-11-17 20:13:21 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    enablePopup: true, // Set to false to disable popup
-    popupTitle: "Start serveru byl odložen",
-    popupMessage: "Start serveru byl odložen z technických důvodů"
-  };
-  
